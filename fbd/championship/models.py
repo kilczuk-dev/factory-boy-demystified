e5e835dceaa21aa93590e0bfc4e843ad7673a760
Piotr Kilczuk <piotr@kilczuk.dev>
2020-05-20 00:09:15 +0200

    Player.picture upload_to

--- fbd/championship/models.py
+++ fbd/championship/models.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -19,6 +21,11 @@ class Club(models.Model):
         return self.name
 
 
+def picture_upload_path(self, filename):
+    basename = os.path.basename(filename)
+    return f'player/picture/{self.id}/{basename}'
+
+
 class Player(models.Model):
     club = models.ForeignKey(Club, models.PROTECT, related_name='players')
     first_name = models.CharField(max_length=100)
@@ -26,7 +33,7 @@ class Player(models.Model):
     dob = models.DateTimeField()
     sex = models.CharField(max_length=100, choices=SEX_CHOICES)
     government_id = models.IntegerField(unique=True)
-    picture = models.ImageField(null=True, blank=True)
+    picture = models.ImageField(null=True, blank=True, upload_to=picture_upload_path)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
